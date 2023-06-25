21bc6dedbd94e9663125a701235e473801ad0f98
Aylin-ai <ajlinkadan3224@gmail.com>
2023-06-25 19:10:36 +0300

    Добавлена страница с данными, связанными с семьей, "Family" и реализован её внешний вид

---
+++ App/ViewModels/FamilyViewModel.cs
@@ -0,0 +1,26 @@
+﻿using App.Infrastructure.Stores;
+using MyApplication.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplication.ViewModels
+{
+    public class FamilyViewModel : ViewModel
+    {
+        private readonly AccountStore _accountStore;
+
+        public FamilyViewModel(AccountStore accountStore, NavigationStore navigationStore)
+        {
+            _accountStore = accountStore;
+
+            #region Команды
+
+
+
+            #endregion
+        }
+    }
+}
