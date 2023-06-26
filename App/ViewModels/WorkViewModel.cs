155c4ce6353549ac28030f74ca27bf7b4ecf66a0
Aylin-ai <ajlinkadan3224@gmail.com>
2023-06-26 13:56:29 +0300

    Создал страницу для документов, связанных со здоровьем, страницу для трудовой книжки и реализовал их внешний вид

---
+++ App/ViewModels/WorkViewModel.cs
@@ -0,0 +1,26 @@
+﻿using App.Infrastructure.Stores;
+using MyApplication.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplication.ViewModels
+{
+    public class WorkViewModel : ViewModel
+    {
+        private readonly AccountStore _accountStore;
+
+        public WorkViewModel(AccountStore accountStore, NavigationStore navigationStore)
+        {
+            _accountStore = accountStore;
+
+            #region Команды
+
+
+
+            #endregion
+        }
+    }
+}
